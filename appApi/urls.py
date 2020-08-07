66d1cd1a6a55fdad35eef97adf6a2837275f5dea
rubensrvsc <Rubensspfc100@gmail.com>
2020-08-07 09:36:04 -0300

    add: aplicaçao funcionando mas ainda falta melhorar a UI

---
+++ appApi/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('api/',ApiResquest,name='api'),
+    path('cadastrar_usuario/', cadastrar_usuario, name="cadastrar_usuario"),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
---
+++ appApi/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import requests
+
+# Create your views here.
+
+@login_required
+def ApiResquest(request):
+    j_son = requests.get('https://api.agify.io?name=michael').json()
+    print(j_son)
+    return render(request,'api.html',{'nomes':j_son})
+
+def cadastrar_usuario(request):
+    if request.method == "POST":
+        form_usuario = UserCreationForm(request.POST)
+        if form_usuario.is_valid():
+            form_usuario.save()
+            return redirect('api')
+    else:
+        form_usuario = UserCreationForm
+    return render(request, 'registrar.html',{'form_usuario':form_usuario})
